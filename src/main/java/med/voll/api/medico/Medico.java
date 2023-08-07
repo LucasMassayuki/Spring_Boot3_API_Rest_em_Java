c810f02f77d8849e22111265782b4fef7f9ec5f7
LucasMassayuki <lucasofficialcruz95@gmail.com>
2023-08-07 20:36:25 -0300

    Nova migration

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -20,7 +20,9 @@ public class Medico {
     private Long id;
     private String nome;
     private String email;
+    private String telefone;
     private String crm;
+
 
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
@@ -32,6 +34,7 @@ public class Medico {
         this.nome = dados.nome();
         this.email = dados.email();
         this.crm = dados.crm();
+        this.telefone = dados.telefone();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
     }
